1c24b2b8d6cdcd861464255b9920efab62e37926
achilleas <kapetanios.achilleas@gmail.com>
2017-02-21 17:36:40 +0200

    Update TweetHud.js

--- public/javascripts/TweetHud.js
+++ public/javascripts/TweetHud.js
@@ -92,11 +92,20 @@ app.controller('TweetHud', function($scope, $resource, $timeout, $rootScope, $ti
 		
 		var state = 'neutral';
 
-  	if (score < 0) {
-  		state = 'negative';
+  	if (score <= -2) {
+  		state = 'super-negative';
   	}
-  	else if (score > 0) {
+		
+	else if (score > -2 && score < 0) {
+  		state = 'negative';
+  	}	
+	
+	else if (score > 0 && score < 2) {
   		state = 'positive';
+  	}
+		
+	else if (score >= 2) {
+  		state = 'super-positive';
   	}
 
 		return state;
