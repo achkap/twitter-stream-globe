10641f03f30e739a0f3ee0898ca675349cfe7e60
achilleas <kapetanios.achilleas@gmail.com>
2017-03-14 21:02:21 +0200

    Update TwitterStreamGlobe.js

--- public/javascripts/TwitterStreamGlobe.js
+++ public/javascripts/TwitterStreamGlobe.js
@@ -53,7 +53,7 @@
 	 */
 	function addEarth () {
 
-	  var sphereGeometry = new THREE.SphereGeometry(600, 50, 50);   // change earth size
+	  var sphereGeometry = new THREE.SphereGeometry(650, 50, 50);   // change earth size
 
 	  var shader = Shaders.earth;
 	  var uniforms = THREE.UniformsUtils.clone(shader.uniforms);
@@ -106,7 +106,7 @@
 	
 	  var phi = PI_HALF - lat * Math.PI / 180 - Math.PI * 0.01;
 	  var theta = 2 * Math.PI - lon * Math.PI / 180 + Math.PI * 0.06;
-	  var rad = 600 + height;  					// change this number if change earth size
+	  var rad = 650 + height;  					// change this number if change earth size
 
 	  vector3.x = Math.sin(phi) * Math.cos(theta) * rad;
 	  vector3.y = Math.cos(phi) * rad;
